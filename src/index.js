9de1c91cda3a1fb7a55af1d4f08bfffa5f9f7f52
Sam Nordlinger <19315410+SNordlinger@users.noreply.github.com>
2020-12-12 14:12:01 -0800

    HORROR SHOW

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-console.log('Hello, world!');
+import snowflake1 from "../assets/snowflake1.png"
+
+function create_snowflake() {
+     const elem = document.createElement('img');
+     elem.setAttribute('src', snowflake1);
+     elem.classList.add('snowflake');
+     const xpos = Math.floor(Math.random() * Math.floor(90));
+     elem.style.right = `${xpos}vw`;
+     return elem;
+}
+
+setInterval(() => {
+     const body = document.querySelector('body');
+     const elem = create_snowflake();
+     body.appendChild(elem)
+}, 1000)
